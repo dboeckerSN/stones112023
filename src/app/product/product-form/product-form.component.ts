13255eb7ca5d49798b2e0871a0318da290eb07de
Dominik Böcker <dominik.boecker@sn-invent.de>
2023-11-22 09:56:17 +0100

    aufg 6.4

--- src/app/product/product-form/product-form.component.ts
+++ src/app/product/product-form/product-form.component.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Product } from '../product';
+import { CustomValidators } from '../../utils/validators/custom-validators';
 
 @Component({
   selector: 'stn-product-form',
@@ -16,16 +22,21 @@ export class ProductFormComponent {
   //   weight: new FormControl(0),
   // });
   productForm = this.fb.group({
-    name: [''],
-    price: [0],
-    weight: [0],
+    name: ['', [Validators.required, CustomValidators.alphaNum]],
+    price: [0, [Validators.required, CustomValidators.positiv]],
+    weight: [0, [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) {}
 
   saveProduct() {
     const formValue = this.productForm.value;
-    if (formValue.name && formValue.price && formValue.weight) {
+    if (
+      this.productForm.valid &&
+      formValue.name &&
+      formValue.price &&
+      formValue.weight
+    ) {
       const product = new Product(
         -1,
         formValue.name,
